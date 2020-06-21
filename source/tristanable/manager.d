23c4e8513422a4b4d87f23d179a5655ab5d8d17e
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-21 17:27:20 +0200

    Added module structure and stub modules.

---
+++ source/tristanable/manager.d
@@ -0,0 +1,4 @@
+module tristanable.manager;
+
+/* TODO: Watcher class to watch for stuff, and add to manager's queues */
+/* TODO: maneger class to use commands on, enqueue and wait for dequeue */
---
+++ source/tristanable/watcher.d
@@ -0,0 +1,4 @@
+module tristanable.watcher;
+
+/* TODO: Watcher class to watch for stuff, and add to manager's queues */
+/* TODO: maneger class to use commands on, enqueue and wait for dequeue */
