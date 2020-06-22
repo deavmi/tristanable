63119aab913e3504e37053a0940c4b3abe6dc346
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-22 22:23:33 +0200

    Added new depepdancy.

--- source/tristanable/manager.d
+++ source/tristanable/manager.d
@@ -4,6 +4,7 @@ import tristanable.watcher : Watcher;
 import tristanable.request : Request;
 import std.socket : Socket;
 import core.sync.mutex : Mutex;
+import bmessage : sendMessage;
 
 /* TODO: Watcher class to watch for stuff, and add to manager's queues */
 /* TODO: maneger class to use commands on, enqueue and wait for dequeue */
@@ -48,6 +49,7 @@ public final class Manager
         byte[] messageData;
 
         /* Send the message */
+
 
         /* Create a new Request */
         Request newRequest = new Request(tag);
--- source/tristanable/watcher.d
+++ source/tristanable/watcher.d
@@ -3,6 +3,7 @@ module tristanable.watcher;
 import tristanable.manager : Manager;
 import std.socket : Socket;
 import core.thread : Thread;
+import bmessage : receiveMessage;
 
 /* TODO: Watcher class to watch for stuff, and add to manager's queues */
 /* TODO: maneger class to use commands on, enqueue and wait for dequeue */
