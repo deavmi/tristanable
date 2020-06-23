0f2bdb9051d4d92ab1e9746fcabedff5a53bee31
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-23 12:46:22 +0200

    Testing updates

--- example/client/source/app.d
+++ example/client/source/app.d
@@ -8,4 +8,10 @@ void main()
 	Socket socket = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
 	socket.connect(parseAddress("127.0.0.1",7777));
 	Manager manager = new Manager(socket);
+
+	manager.sendMessage(69, [77]);
+
+
+	byte[] receivedKaka = manager.receiveMessage(69);
+	writeln(receivedKaka);
 }
