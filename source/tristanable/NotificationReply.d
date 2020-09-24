4623cbd30446c5d0b5895df9a3a7d132883ccf15
Tristan 🅱. Kildaire <21629986@sun.ac.za>
2020-09-24 20:31:15 +0200

    Added NotificationReply class to represent an incoming notification message

---
+++ source/tristanable/NotificationReply.d
@@ -0,0 +1,22 @@
+/**
+* NotificationReply
+*
+* When a tag is reserved and a message is received
+* with such a tag then one of these is generated
+* and added to the queue of notification replies.
+*
+* Multiple of these will be made and enqueued even
+* if they have the same tag (duplicates allowed).
+*
+* This facilitates a notification system if one
+* wants to use tristanable for that purpose (this
+* is because notifications _just happen_ and have
+* no prior request)
+*/
+
+module tristanable.notifications;
+
+public class NotificationReply
+{
+	
+}
