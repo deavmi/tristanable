7ba3c9f1f7ee198738aa954cae8a2b3e24e383aa
Tristan B. Kildaire <deavmi@redxen.eu>
2021-09-08 13:49:41 +0200

    Added package.d

---
+++ source/tristanable/package.d
@@ -0,0 +1,7 @@
+module tristanable;
+
+public import tristanable.encoding;
+public import tristanable.manager;
+public import tristanable.queue;
+public import tristanable.queueitem;
+public import tristanable.watcher;
