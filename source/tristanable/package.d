cbfefabb06f30271919d787f9e370fdb5615fa53
Tristan B. Kildaire <deavmi@redxen.eu>
2022-05-17 14:05:00 +0200

    Added exceptions to package import path

--- source/tristanable/package.d
+++ source/tristanable/package.d
@@ -4,4 +4,7 @@ public import tristanable.encoding;
 public import tristanable.manager;
 public import tristanable.queue;
 public import tristanable.queueitem;
+
+/* FIXME: We need not publically import watcher, EVER! */
 public import tristanable.watcher;
+public import tristanable.exceptions;
