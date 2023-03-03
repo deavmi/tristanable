01a5d779c835e530948449c1ebaa05b8c7f066d5
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-03 17:49:32 +0200

    Package
    - Added a title to the documentation to better describe what this library does
    
    Dub
    
    - Updated package description
    - Added project website link

--- source/tristanable/package.d
+++ source/tristanable/package.d
@@ -1,4 +1,8 @@
+/**
+ * Tristanable network message queuing framework
+ */
 module tristanable;
+
 
 public import tristanable.manager : Manager;
 public import tristanable.queue : Queue, QueueItem;
