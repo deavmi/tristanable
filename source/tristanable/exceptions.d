07d47551aed0107edca71f0155345e9e7b5fb3bd
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-06 15:46:34 +0200

    Exceptions
    - Documented `ErrorType` and all its members
    - Documented `TristanableException`

--- source/tristanable/exceptions.d
+++ source/tristanable/exceptions.d
@@ -3,13 +3,32 @@
  */
 module tristanable.exceptions;
 
+/** 
+ * The type of sub-error of the `TristanableException`
+ */
 public enum ErrorType
 {
+    /**
+     * If the requested queue could not be found
+     */
     QUEUE_NOT_FOUND,
+
+    /**
+     * If the queue wanting to be registered has already
+     * been registered under the same tag
+     */
     QUEUE_ALREADY_EXISTS,
+
+    /** 
+     * If no default queue is configured
+     */
     NO_DEFAULT_QUEUE
 }
 
+/** 
+ * Any sort of error that occurs during runtime of the tristanable
+ * engine
+ */
 public class TristanableException : Exception
 {
     private ErrorType err;
