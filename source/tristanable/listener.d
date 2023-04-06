f39d65f73ffa698c8724127bfe72a5a860a9cdea
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-06 09:04:09 +0200

    Listener
    - Added `TListener` interface

--- source/tristanable/listener.d
+++ source/tristanable/listener.d
@@ -1,3 +1,12 @@
 module tristanable.listener;
 
 // TODO: Implement me
+
+import tristanable.queue;
+import tristanable.encoding;
+
+public interface TListener
+{
+    // TODO: See if this is all we need / what we want
+    public void onQueueReceive(Queue queue, TaggedMessage message);
+}
